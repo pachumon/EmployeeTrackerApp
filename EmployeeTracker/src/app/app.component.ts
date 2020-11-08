0bb97d6400e7d66e1e836eef296970df38b89e6a
prasanth <shenoy.prasanth@gmail.com>
2020-11-08 10:12:29 +0000

    Initial Commit with the basic packages included and the app layout setup

---
+++ EmployeeTracker/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'fai-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'EmployeeTracker';
+}
